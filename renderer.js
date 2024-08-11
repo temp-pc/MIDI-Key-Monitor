25dcc6c9d0ceffdd94e6eb7c6c4135a097ef973c
N thms <thms@enunoMacBook-Air.local>
2024-08-11 20:45:08 +0900

    piano width

--- renderer.js
+++ renderer.js
@@ -1,3 +1,44 @@
+
+
+let pianoContainer = document.getElementsByClassName("piano-container");
+window.onload = () => {
+  const pianoPadding = 1; //単位：em
+  const keys = 13;
+  const quotient = Math.floor(keys / 12), remainder = keys % 12;
+  let black_keys_count = remainder < 2 ? 0 : remainder < 4 ? 1 : remainder < 7 ? 2 : remainder < 9 ? 3 : remainder < 11 ? 4 : 5 ;
+  black_keys_count = quotient * 5 + black_keys_count;
+  console.log("quotient : " + quotient + " remainder : " + remainder + " black_keys : " + black_keys_count)
+
+  //24keys
+  for (let index = 1; index <= keys; index++) {
+    let div = document.createElement("div");
+    div.classList.add("key", index <= black_keys_count ? "black-key" : "white-key");
+    let emNum = 0;
+    if(index <= black_keys_count){
+      if(index == 1){
+        emNum = 3;
+      }else if(index == 2){
+        emNum = 8;
+      }else if(index == 3){
+        emNum = 15;
+      }else if(index == 4){
+        emNum = 20;
+      }else if(index == 5){
+        emNum = 25;
+      }else if(index == 6){
+        emNum = 25;
+      }
+      div.style.left = `${emNum}em`
+    }
+    //For playing audio on click
+    // const number = index <= black_keys_count - 1 ? "0" + index : index;
+    // div.addEventListener("click", () => {
+    //   new Audio(`${base}key${number}.mp3`).play();
+    // });
+    pianoContainer[0].appendChild(div);
+  }
+};
+
 if (navigator.requestMIDIAccess) {
   navigator.requestMIDIAccess()
       .then(onMIDISuccess, onMIDIFailure);
