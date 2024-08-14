f8dded50663b2ad39c1760723d075cbbeab21c26
N I_temp <crts5fld@gmail.com>
2024-08-14 18:51:42 +0900

    aaa

--- renderer.js
+++ renderer.js
@@ -1,54 +1,109 @@
 
+const pianoContainer = document.querySelector("#piano-container");
 
-let pianoContainer = document.getElementsByClassName("piano-container");
-window.onload = () => {
-  const pianoPadding = 1; //単位：em
-  const keys = 13;
-  const quotient = Math.floor(keys / 12), remainder = keys % 12;
-  let black_keys_count = remainder < 2 ? 0 : remainder < 4 ? 1 : remainder < 7 ? 2 : remainder < 9 ? 3 : remainder < 11 ? 4 : 5 ;
+const numberOfKeysSelector = document.querySelector("#numberOfKeysSelector");
+for (let i = 1; i <= 100; i++) {
+  const option = document.createElement("option");
+  option.value = i;
+  option.textContent = i;
+  numberOfKeysSelector.appendChild(option);
+}
+
+initialNumberOfKeys = 24;
+numberOfKeysSelector.value = initialNumberOfKeys;  //キーボードの鍵盤数の初期値を設定。
+createKeys(initialNumberOfKeys)
+
+numberOfKeysSelector.addEventListener("change", (event) => {
+  const selectedValue = parseInt(event.target.value);
+  createKeys(selectedValue);
+})
+
+
+
+function createKeys(numberOfKeys) {
+  const quotient = Math.floor(numberOfKeys / 12), remainder = numberOfKeys % 12;
+  let black_keys_count = remainder < 2 ? 0 : remainder < 4 ? 1 : remainder < 7 ? 2 : remainder < 9 ? 3 : remainder < 11 ? 4 : 5;
   black_keys_count = quotient * 5 + black_keys_count;
   console.log("quotient : " + quotient + " remainder : " + remainder + " black_keys : " + black_keys_count)
 
+  const blackKeyWidth = 2.5;  // 単位はem
+  const GapOfBlackKey = 0.5; //黒鍵の位置を真ん中から少しずらす
+  const whiteKeyWidth = 4;
+  const pianoContainerPadding = 1;
+
   //24keys
-  for (let index = 1; index <= keys; index++) {
+  // for (let num = 1; num <= numberOfKeys; num++) {
+  //   let div = document.createElement("div");
+  //   div.classList.add("key", num <= black_keys_count ? "black-key" : "white-key");
+
+  //   let blackKeyIndex = ( num + 5 ) % 5;
+  //   let blackKeyPosition = 0;
+  //   if(num <= black_keys_count){
+
+  //     if(blackKeyIndex == 1){
+  //       blackKeyPosition = ( whiteKeyWidth * num ) - ( blackKeyWidth / 2 ) - GapOfBlackKey;
+  //     }else if(blackKeyIndex == 2){
+  //       blackKeyPosition = ( whiteKeyWidth * num ) - ( blackKeyWidth / 2 ) + GapOfBlackKey;
+  //     }else if(blackKeyIndex == 3){
+  //       blackKeyPosition = ( whiteKeyWidth * (num+1) ) - ( blackKeyWidth / 2 ) - GapOfBlackKey;
+  //     }else if(blackKeyIndex == 4){
+  //       blackKeyPosition = ( whiteKeyWidth * (num+1) ) - ( blackKeyWidth / 2 );
+  //     }else if(blackKeyIndex == 0){
+  //       blackKeyPosition = ( whiteKeyWidth * (num+1) ) - ( blackKeyWidth / 2 ) + GapOfBlackKey;
+  //     }
+
+  //     div.style.width = `${blackKeyWidth}em`;
+  //     div.style.left = `${blackKeyPosition + pianoContainerPadding}em`
+  //     div.id = `black-key-${num}`
+
+  //   }else{
+  //     div.style.width = `${whiteKeyWidth}em`;
+  //   }
+  //   pianoContainer.appendChild(div);
+  // }
+  for (let num = 1; num <= numberOfKeys; num++) {
     let div = document.createElement("div");
-    div.classList.add("key", index <= black_keys_count ? "black-key" : "white-key");
-    let emNum = 0;
-    if(index <= black_keys_count){
-      if(index == 1){
-        emNum = 3;
-      }else if(index == 2){
-        emNum = 8;
-      }else if(index == 3){
-        emNum = 15;
-      }else if(index == 4){
-        emNum = 20;
-      }else if(index == 5){
-        emNum = 25;
-      }else if(index == 6){
-        emNum = 25;
+
+    keyIndex = (num - 1) % 12
+    const blackKeyIndexList = [1, 3, 6, 8, 10];
+    if (blackKeyIndexList.includes(keyIndex)) {
+      console.log(keyIndex);
+      div.classList.add("black-key");
+      div.style.width = `${blackKeyWidth}em`;
+      if (keyIndex == 1) {
+        blackKeyPosition = (whiteKeyWidth * num) - (blackKeyWidth / 2) - GapOfBlackKey;
+      } else if (keyIndex == 2) {
+        blackKeyPosition = (whiteKeyWidth * num) - (blackKeyWidth / 2) + GapOfBlackKey;
+      } else if (keyIndex == 3) {
+        blackKeyPosition = (whiteKeyWidth * (num + 1)) - (blackKeyWidth / 2) - GapOfBlackKey;
+      } else if (keyIndex == 4) {
+        blackKeyPosition = (whiteKeyWidth * (num + 1)) - (blackKeyWidth / 2);
+      } else if (keyIndex == 0) {
+        blackKeyPosition = (whiteKeyWidth * (num + 1)) - (blackKeyWidth / 2) + GapOfBlackKey;
       }
-      div.style.left = `${emNum}em`
+      div.style.left = `${blackKeyPosition + pianoContainerPadding}em`
+      div.id = `black-key-${num}`
+    } else {
+      console.log(keyIndex);
+      div.classList.add("white-key");
+      div.style.width = `${whiteKeyWidth}em`;
     }
-    //For playing audio on click
-    // const number = index <= black_keys_count - 1 ? "0" + index : index;
-    // div.addEventListener("click", () => {
-    //   new Audio(`${base}key${number}.mp3`).play();
-    // });
-    pianoContainer[0].appendChild(div);
+
+    pianoContainer.appendChild(div);
   }
-};
+}
+
 
 if (navigator.requestMIDIAccess) {
   navigator.requestMIDIAccess()
-      .then(onMIDISuccess, onMIDIFailure);
+    .then(onMIDISuccess, onMIDIFailure);
 } else {
   console.log("WebMIDI is not supported in this browser.");
 }
 
 function onMIDISuccess(midiAccess) {
   midiAccess.inputs.forEach(function (input) {
-      input.onmidimessage = onMIDIMessage;
+    input.onmidimessage = onMIDIMessage;
   });
 }
 
@@ -63,9 +118,9 @@ function onMIDIMessage(message) {
   const key = document.querySelector(`.key[data-note="${note}"]`);
 
   if (isNoteOn) {
-      key.classList.add('active');
+    key.classList.add('active');
   } else if (isNoteOff) {
-      key.classList.remove('active');
+    key.classList.remove('active');
   }
 }
 
