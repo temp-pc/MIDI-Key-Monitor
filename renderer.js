9ac0b115b43e1a9dfe70f85b66fda995169a2475
N I_temp <crts5fld@gmail.com>
2024-08-17 18:53:20 +0900

    aeae

--- renderer.js
+++ renderer.js
@@ -1,96 +1,125 @@
+const noteCharList = [ "A", "As", "B", "C", "Cs", "D", "Ds", "E", "F", "Fs", "G", "Gs"];
+const baseNote = {
+  "C": 0,
+  "Cs": 1,
+  "D": 2,
+  "Ds": 3,
+  "E": 4,
+  "F": 5,
+  "Fs": 6,
+  "G": 7,
+  "Gs": 8,
+  "A": 9,
+  "As": 10,
+  "B": 11
+};
 
 const pianoContainer = document.querySelector("#piano-container");
-
+const pianoBackground = document.querySelector('#piano-background');
 const numberOfKeysSelector = document.querySelector("#numberOfKeysSelector");
-for (let i = 1; i <= 100; i++) {
+for (let i = 1; i <= 88; i++) {
   const option = document.createElement("option");
   option.value = i;
   option.textContent = i;
   numberOfKeysSelector.appendChild(option);
 }
-
-initialNumberOfKeys = 24;
-numberOfKeysSelector.value = initialNumberOfKeys;  //キーボードの鍵盤数の初期値を設定。
-createKeys(initialNumberOfKeys)
-
 numberOfKeysSelector.addEventListener("change", (event) => {
   const selectedValue = parseInt(event.target.value);
   createKeys(selectedValue);
 })
 
 
+let octaveShiftStatus = 0;
+const initialNumberOfKeys = 24;
+numberOfKeysSelector.value = initialNumberOfKeys;  //キーボードの鍵盤数の初期値を設定。
+
+const initialFirstNotePitch = "A3";
+
+function noteToMidi(note) {
+  const match = note.match(/^([A-G][s]?)(\d)$/);
+  if (!match) return null;
+
+  const [ , pitch, octave ] = match;
+  const pitchIndex = noteCharList.indexOf(pitch);
+  if (pitchIndex === -1) return null;
+
+  return (parseInt(octave) + 1) * 12 + baseNote[pitch] ;
+}
+function midiToNote(midiNumber) {
+  const octave = Math.floor(midiNumber / 12) - 1;
+  const pitchIndex = midiNumber % 12;
+
+  const pitch = noteCharList.find((n, index) => baseNote[n] === pitchIndex);
+  if (!pitch) return null;
+
+  return `${pitch}${octave}`;
+}
+
+
+createKeys(initialNumberOfKeys);
+
 
 function createKeys(numberOfKeys) {
-  const quotient = Math.floor(numberOfKeys / 12), remainder = numberOfKeys % 12;
-  let black_keys_count = remainder < 2 ? 0 : remainder < 4 ? 1 : remainder < 7 ? 2 : remainder < 9 ? 3 : remainder < 11 ? 4 : 5;
-  black_keys_count = quotient * 5 + black_keys_count;
-  console.log("quotient : " + quotient + " remainder : " + remainder + " black_keys : " + black_keys_count)
-
-  const blackKeyWidth = 2.5;  // 単位はem
-  const GapOfBlackKey = 0.5; //黒鍵の位置を真ん中から少しずらす
-  const whiteKeyWidth = 4;
+  pianoBackground.innerHTML = ''; //初期化
+ 
+  const whiteKeyWidth = 3.2; // 単位はem
+  const blackKeyWidth = whiteKeyWidth * 0.6;
+  const blackKeyWidthhalf = blackKeyWidth / 2;
+  const GapOfBlackKey = whiteKeyWidth / 8; //黒鍵の位置を真ん中から少しずらす
   const pianoContainerPadding = 1;
+  const blackKeyIndexList = [2, 4, 7, 9, 11];
+  const oneOctaveWidth = whiteKeyWidth * 7;
+  const firstNotePitch = "C4";
+  const firstNoteMidiNumber = noteToMidi(firstNotePitch);
+  const firstNoteMatch = firstNotePitch.match(/^([A-G][s]?)(\d)$/);
+  let whiteKeyCount = 0;
+  const [ , firstNoteChar, firstNoteOctave ] = firstNoteMatch;
+  let pianoBackgroundWidth = 0;
 
-  //24keys
-  // for (let num = 1; num <= numberOfKeys; num++) {
-  //   let div = document.createElement("div");
-  //   div.classList.add("key", num <= black_keys_count ? "black-key" : "white-key");
-
-  //   let blackKeyIndex = ( num + 5 ) % 5;
-  //   let blackKeyPosition = 0;
-  //   if(num <= black_keys_count){
-
-  //     if(blackKeyIndex == 1){
-  //       blackKeyPosition = ( whiteKeyWidth * num ) - ( blackKeyWidth / 2 ) - GapOfBlackKey;
-  //     }else if(blackKeyIndex == 2){
-  //       blackKeyPosition = ( whiteKeyWidth * num ) - ( blackKeyWidth / 2 ) + GapOfBlackKey;
-  //     }else if(blackKeyIndex == 3){
-  //       blackKeyPosition = ( whiteKeyWidth * (num+1) ) - ( blackKeyWidth / 2 ) - GapOfBlackKey;
-  //     }else if(blackKeyIndex == 4){
-  //       blackKeyPosition = ( whiteKeyWidth * (num+1) ) - ( blackKeyWidth / 2 );
-  //     }else if(blackKeyIndex == 0){
-  //       blackKeyPosition = ( whiteKeyWidth * (num+1) ) - ( blackKeyWidth / 2 ) + GapOfBlackKey;
-  //     }
-
-  //     div.style.width = `${blackKeyWidth}em`;
-  //     div.style.left = `${blackKeyPosition + pianoContainerPadding}em`
-  //     div.id = `black-key-${num}`
-
-  //   }else{
-  //     div.style.width = `${whiteKeyWidth}em`;
-  //   }
-  //   pianoContainer.appendChild(div);
-  // }
   for (let num = 1; num <= numberOfKeys; num++) {
-    let div = document.createElement("div");
+    const div = document.createElement("div");
+    const noteMidiNumber = firstNoteMidiNumber + num - 1;
+    const notePitch = midiToNote(noteMidiNumber);
+    const match = notePitch.match(/^([A-G][s]?)(\d)$/);
+    const [ , noteChar, noteOctave ] = match;
+    
+    div.setAttribute("data-note", noteMidiNumber);
+    div.classList.add("key");
+    div.id = notePitch;
 
-    keyIndex = (num - 1) % 12
-    const blackKeyIndexList = [1, 3, 6, 8, 10];
-    if (blackKeyIndexList.includes(keyIndex)) {
-      console.log(keyIndex);
+    keyIndex = num % 12;
+    const octave = Math.floor( num / 12) + 1;
+
+    if (noteChar.includes("s")) {
+
       div.classList.add("black-key");
       div.style.width = `${blackKeyWidth}em`;
-      if (keyIndex == 1) {
-        blackKeyPosition = (whiteKeyWidth * num) - (blackKeyWidth / 2) - GapOfBlackKey;
-      } else if (keyIndex == 2) {
-        blackKeyPosition = (whiteKeyWidth * num) - (blackKeyWidth / 2) + GapOfBlackKey;
-      } else if (keyIndex == 3) {
-        blackKeyPosition = (whiteKeyWidth * (num + 1)) - (blackKeyWidth / 2) - GapOfBlackKey;
-      } else if (keyIndex == 4) {
-        blackKeyPosition = (whiteKeyWidth * (num + 1)) - (blackKeyWidth / 2);
-      } else if (keyIndex == 0) {
-        blackKeyPosition = (whiteKeyWidth * (num + 1)) - (blackKeyWidth / 2) + GapOfBlackKey;
+
+      let blackKeyPosition = whiteKeyWidth * whiteKeyCount - blackKeyWidthhalf;
+
+      // ギャップの調整
+      if (noteChar === "Cs" || noteChar === "Fs") {
+        blackKeyPosition -= GapOfBlackKey;
+      }else if(noteChar === "Ds" || noteChar === "As"){
+        blackKeyPosition += GapOfBlackKey;
       }
-      div.style.left = `${blackKeyPosition + pianoContainerPadding}em`
-      div.id = `black-key-${num}`
+      div.style.left = `${blackKeyPosition}em`;
+
+      //黒鍵で終わってたら、その分piano-containerのサイズを大きくする
+      if(num == 1 || num == numberOfKeys){
+        pianoBackgroundWidth += blackKeyWidth;
+      }
     } else {
-      console.log(keyIndex);
+
       div.classList.add("white-key");
       div.style.width = `${whiteKeyWidth}em`;
+      whiteKeyCount += 1;
+      pianoBackgroundWidth += whiteKeyWidth;
     }
 
-    pianoContainer.appendChild(div);
+    pianoBackground.appendChild(div);
   }
+  pianoBackground.style.width = `${pianoBackgroundWidth}em`;
 }
 
 
